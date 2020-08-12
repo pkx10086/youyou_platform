1d1f10bdb9d29156bb80c959a7dcd737b5b8074b
pankx <pankx@asiainfo.com>
2020-08-12 16:20:32 +0800

    统一异常管理，统一验证

--- youyou_nacos_discovery/src/main/java/com/youyou/platform/NacosDiscoveryApplication.java
+++ youyou_nacos_discovery/src/main/java/com/youyou/platform/NacosDiscoveryApplication.java
@@ -14,5 +14,6 @@ public class NacosDiscoveryApplication
 {
     public static void main( String[] args )
     {
+        System.out.print("1111111111");
         SpringApplication.run(NacosDiscoveryApplication.class, args);    }
 }
